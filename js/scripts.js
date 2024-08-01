c145967d6370bb3c929f3c8afdb052cdf218fe28
millemnm <maxianmiller@gmail.com>
2024-08-01 12:01:51 -0700

    Update 2

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,10 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shrink');
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink');
         }
-
     };
 
     // Shrink the navbar 
